65cd64e91eaedc976918241f1e46a5fd51b319d5
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-01-13 17:45:22 +0200

    Adding Guard.

--- C# Web Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
+++ C# Web Basics/BasicWebServer/BasicWebServer.Server/HTTP/Header.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BasicWebServer.Server.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace BasicWebServer.Server.HTTP
     {
         public Header(string name, string value)
         {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
             Name = name;
             Value = value;
         }
