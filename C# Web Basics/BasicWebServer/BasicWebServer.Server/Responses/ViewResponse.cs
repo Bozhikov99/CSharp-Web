dc69f1644279d4edaab14e397a65a5f12adc10b4
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-01-29 17:08:42 +0200

    Introducing ViewResponse.cs

---
+++ C# Web Basics/BasicWebServer/BasicWebServer.Server/Responses/ViewResponse.cs
@@ -0,0 +1,27 @@
+﻿using BasicWebServer.Server.HTTP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class ViewResponse : ContentResponse
+    {
+        private const char PathSeparator = '/';
+
+        public ViewResponse(string viewName, string controller)
+            : base("", ContentType.Html)
+        {
+            if (!viewName.Contains(PathSeparator))
+            {
+                viewName = $"{controller}{PathSeparator}{viewName}";
+            }
+
+            string viewPath = Path.GetFullPath($"./Views/" +
+                $"{viewName.TrimStart(PathSeparator)}" +
+                $".cshtml");
+        }
+    }
+}
